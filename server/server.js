b7419babcb756573195769769de216542076f753
Vámos László <laszlo.vamos@process-solutions.hu>
2016-01-10 13:50:58 +0100

    init

---
+++ server/server.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+var app = express();
+var path = require("path");
+var port = process.env.PORT || 7200;
+
+var rootFolder = path.join(__dirname, '../client/');
+
+app.use('/client', express.static('client'));
+app.use('/bower_components', express.static('bower_components'));
+
+app.get('/', function (req, res){
+    res.sendFile('index.html', { root: rootFolder });
+})
+app.listen(port, function () {
+    console.log('Express server listening on port ' + port);
+});
